4fbda1b907bcaa01a3ae90744b751d148203b305
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-14 16:15:41 +0200

    Front end done

--- client/main.js
+++ client/main.js
@@ -1,8 +1,23 @@
 console.log('Client Script Ready:');
 
 const form = document.querySelector('form');
+const loading = document.querySelector('.loading');
+
+loading.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); 
-    console.log('Form Submitted')
+    console.log('Form Submitted');
+
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const post = {
+        name,
+        content
+    }
+    console.log(post);
+    form.style.display = 'none';
+    loading.style.display = 'block';
 })
