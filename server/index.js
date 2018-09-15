308a74a3eca0cc4ddf41e41082c72991c5cfab73
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-15 13:18:12 +0200

    Post insert Validation - And DB preparation with MONK

--- server/index.js
+++ server/index.js
@@ -1,19 +1,49 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Handle CORS related issues
+const monk = require('monk'); // DB
 
-const app = express();
+const app = express(); 
+
+const monk = monk('localhost/messenger18'); // Connects to DB
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Parses client side responds into json
 
 app.get('/', (req, res) => {
+  // Server respond
   res.json({
     message: 'Hello from Express'
   });
 });
 
+function isPostValid(post) {
+  //   SHORT-HAND expression:
+  //   return post.name && post.name.toString().trim() !== '' &&
+  //   post.content && post.content.toString().trim() !== '';
+
+  if (!post.name || post.name.toString().trim() == '') {
+    return false;
+  } else if (!post.content || post.content.toString().trim() == '') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 app.post('/post', (req, res) => {
-    console.log(req.body);
+  // Incoming posts from client
+  if (isPostValid(req.body)) {
+    const post = {
+      name: req.body.name.toString(), // toString prevents DATA INJECTION
+      content: req.body.content.toString()
+    };
+    console.log('Valid post!', post);
+  } else {
+    res.status(422);
+    res.json({
+      message: 'Missing required fields!'
+    });
+  }
 });
 
 app.listen(5000, () => {
